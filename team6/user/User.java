8d191c431b0efeddc2e59cb832055b346fd05ae9
Cho2018 <44170716+Cho2018@users.noreply.github.com>
2020-03-05 23:21:57 +0900

    Add files via upload

---
+++ team6/user/User.java
@@ -0,0 +1,40 @@
+package user;
+
+public class User {
+    private String userID;
+    private String password;
+    private int age;
+    private String job;
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
